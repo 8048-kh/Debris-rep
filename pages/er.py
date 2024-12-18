d9ed9f3757dc46701ff69f0c2dac19adf3e105f1
8048-kh <8046khtotn@gmail.com>
2024-12-18 21:29:32 +0800

    Update er.py

--- pages/er.py
+++ pages/er.py
@@ -21,16 +21,17 @@ tribe_names = tribes_df['tribe name'].tolist()
 # Create a selectbox for tribe names
         
 selected_tribe = st.selectbox(
-    "選擇部落",  # Label for the selectbox
-    tribe_names,  # Options for the selectbox (tribe names)
-    key="selectbox_tribe"  # Unique key for the selectbox
+            "選擇部落",  # Label for the selectbox
+            tribe_names,  # Options for the selectbox (tribe names)
+            key="selectbox_tribe"  # Unique key for the selectbox
     )
 selected_tribe_data = tribes_df[tribes_df['tribe name'] == selected_tribe].iloc[0]
 latitude = selected_tribe_data['latitude']
 longitude = selected_tribe_data['longitude']
 
 # Update map center
-m.center = (latitude, longitude)  
+#m.center = (latitude, longitude)  
+m.center = (longitude, latitude)
 m.zoom = 12
 # Display the updated map and selected tribe
 m.to_streamlit(height=700)
