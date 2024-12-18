70a98ac9417691b7b2adfff331829b9d479f6968
8048-kh <8046khtotn@gmail.com>
2024-12-18 22:11:15 +0800

    Update debris.py

--- pages/debris.py
+++ pages/debris.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -10,22 +11,37 @@ A Streamlit map template
 
 st.title("Aboriginal Tribes")
 
-with st.expander("See source code"):
-    with st.echo():
 
-        m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
-        cities = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/nantou%20tribe.csv"
-        regions = "https://github.com/8048-kh/test/raw/refs/heads/main/REGION.shp"
 
-        m.add_shp(regions, layer_name="Aboriginal Tribes")
-        m.add_points_from_xy(
-            cities,
+m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
+tribes = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Nantou%20Tribe.csv"
+tribes_df = pd.read_csv(tribes)
+tribe_names = tribes_df['tribe name'].tolist()
+
+m.add_points_from_xy(
+            tribes,
             x="longitude",
             y="latitude",
-            #color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
             add_legend=True,
         )
 
+# Create a selectbox for tribe names
+        
+selected_tribe = st.selectbox(
+    "選擇部落", tribe_names, key="selectbox_tribe"
+)
+selected_tribe_data = tribes_df[tribes_df['tribe name'] == selected_tribe].iloc[0]
+
+# 從 'latitude' 和 'longitude' 欄位讀取座標
+latitude = selected_tribe_data['latitude']
+longitude = selected_tribe_data['longitude']
+
+# 更新地圖中心和縮放級別
+m.center = (latitude, longitude)  
+m.zoom = 14  # 設定縮放級別為 12 或任何你想要的級別
 m.to_streamlit(height=700)
+
+# Display the selected tribe
+st.write(f"您選擇的部落是：{selected_tribe}")
