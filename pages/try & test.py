2640cfa99a3c0d4e352bf74f14657c269b5838d6
8048-kh <8046khtotn@gmail.com>
2024-12-19 20:24:46 +0800

    Update try & test.py

--- pages/try & test.py
+++ pages/try & test.py
@@ -41,7 +41,7 @@ selected_tribe_data = tribes_df[tribes_df['tribe name'] == selected_tribe].iloc[
 # Get coordinates from 'latitude' and 'longitude'
 latitude = selected_tribe_data['latitude']
 longitude = selected_tribe_data['longitude']
-#m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/tribe_test.shp")
+m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/tribe_test.shp")
 m.add_geojson(debris, layer_name='debris')
 # Recenter and zoom to the selected tribe
 #m.set_center(latitude, longitude, zoom=12)  # Dynamically update center and zoom level
