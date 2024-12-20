afb932a258c6d0dc02fc75e9f9a3bf871a416af1
8048-kh <8046khtotn@gmail.com>
2024-12-20 18:47:40 +0800

    Update debris.py

--- pages/debris.py
+++ pages/debris.py
@@ -15,21 +15,12 @@ st.title("Aboriginal Tribes")
 m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
 
 # Load the tribes data
-tribes = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Nantou_Tribe.csv"
-debris = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/debris_impact.geojson"
+tribes = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Nantou_Tribe.csv"
+debris = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/debris_impact.geojson"
 tribes_df = pd.read_csv(tribes)
 tribe_names = tribes_df['tribe name'].tolist()
 
 # Add points to the map
-m.add_points_from_xy(
-    tribes,
-    x="longitude",
-    y="latitude",
-    #icon_names=["gear", "map", "leaf", "globe"],
-    spin=True,
-    #add_legend=True,
-)
-
 # Create a selectbox for tribe names
 selected_tribe = st.selectbox(
     "選擇部落", tribe_names, key="selectbox_tribe"
@@ -41,8 +32,8 @@ selected_tribe_data = tribes_df[tribes_df['tribe name'] == selected_tribe].iloc[
 # Get coordinates from 'latitude' and 'longitude'
 latitude = selected_tribe_data['latitude']
 longitude = selected_tribe_data['longitude']
-m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/tribetest/tribe_test2_5.shp")
-m.add_geojson(debris, layer_name='debris')
+m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/tribetest/tribes_p1.shp")
+#m.add_geojson(debris, layer_name='debris')
 m.add_geojson(
     debris,
     layer_name='debris',
@@ -62,12 +53,22 @@ m.add_geojson(
     },
     add_legend=True,
 )
+legend_dict = {
+    "持續觀察": "green",
+    "低": "yellow",
+    "中": "orange",
+    "高": "red",
+}
 
+m.add_legend(
+    title="Risk Level",
+    legend_dict=legend_dict,
+    opacity=1.0,
+    position="bottomright",
+)
 
 # Recenter and zoom to the selected tribe
-#m.set_center(latitude, longitude, zoom=12)  # Dynamically update center and zoom level
 m.set_center(longitude, latitude, zoom=15) 
-# Add a marker for the selected tribe
 m.add_marker(location=(latitude, longitude), tooltip=selected_tribe, popup=f"{selected_tribe}")
 # Display the map in Streamlit
 st.write(f"您選擇的部落是：{selected_tribe}")
