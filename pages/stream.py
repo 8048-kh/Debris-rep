d1653c16634e94dde72dbeb52828a98c77c69c7c
8048-kh <8046khtotn@gmail.com>
2024-12-21 23:18:00 +0800

    Update stream.py

--- pages/stream.py
+++ pages/stream.py
@@ -32,7 +32,7 @@ selected_tribe_data = tribes_df[tribes_df['tribe name'] == selected_tribe].iloc[
 # Get coordinates from 'latitude' and 'longitude'
 latitude = selected_tribe_data['latitude']
 longitude = selected_tribe_data['longitude']
-m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/tribetest/tribes_p1.shp")
+m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Full_Nantou_Tribe.shp")
 gdf = gpd.read_file("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/streams.geojson")  # 替換為您的 GeoJSON 檔案路徑
 
 color_dict = {
