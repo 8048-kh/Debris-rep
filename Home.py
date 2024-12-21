6819917ed87ba10c8a95107cedfec0489cb499c1
8048-kh <8046khtotn@gmail.com>
2024-12-21 12:43:57 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,21 +15,20 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("南投原鄉部落與土石流分布")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    南投原鄉部落與土石流潛勢溪流、土石流潛勢溪流範圍分布
     """
 )
 
-st.header("Instructions")
+st.header("目錄")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+1. 原鄉部落座標與資訊
+2. 原鄉部落與土石流潛勢溪流
+3. 原鄉部落與土石流潛勢溪流範圍
 
 """
 
