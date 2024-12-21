e904765abea943d587e35a979b99d1e86add8bb4
8048-kh <8046khtotn@gmail.com>
2024-12-21 12:08:42 +0800

    Update stream.py

--- pages/stream.py
+++ pages/stream.py
@@ -16,7 +16,6 @@ m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
 
 # Load the tribes data
 tribes = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Nantou_Tribe.csv"
-debris = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/debris_impact.geojson"
 tribes_df = pd.read_csv(tribes)
 tribe_names = tribes_df['tribe name'].tolist()
 
@@ -34,7 +33,39 @@ latitude = selected_tribe_data['latitude']
 longitude = selected_tribe_data['longitude']
 m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/tribetest/tribes_p1.shp")
 m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/streams.shp")
+shp_file = gpd.read_file("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/shpfile/streams.shp")
+risk = shp_file['Risk']
+m.add_shp(
+    layer_name='streams',
+    style_callback=lambda feature: {
+        "fillColor": (
+            "orange"
+            if feature["properties"]["Risk"] == "中"
+            else "yellow"
+            if feature["properties"]["Risk"] == "低"
+            else "green"
+            if feature["properties"]["Risk"] == "持續觀察"
+            else "red"
+        ),
+        "color": "black",
+        "weight": 1,
+        "fillOpacity": 0.5,
+    },
+    add_legend=True,
+)
+legend_dict = {
+    "持續觀察": "green",
+    "低": "yellow",
+    "中": "orange",
+    "高": "red",
+}
 
+m.add_legend(
+    title="Risk Level",
+    legend_dict=legend_dict,
+    opacity=1.0,
+    position="bottomright",
+)
 # Recenter and zoom to the selected tribe
 m.set_center(longitude, latitude, zoom=15) 
 m.add_marker(location=(latitude, longitude), tooltip=selected_tribe, popup=f"{selected_tribe}")
