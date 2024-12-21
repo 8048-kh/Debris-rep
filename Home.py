0ddfbb7c8fa67f6712a05bf071f9b572d208e0be
8048-kh <8046khtotn@gmail.com>
2024-12-21 12:47:31 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -37,7 +37,7 @@ st.markdown(markdown)
 
 m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
 tribes = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Nantou_Tribe.csv"
-m.add_geojson(regions, layer_name='tribes')
+m.add_geojson(tribes, layer_name='tribes')
 m.add_points_from_xy(
             tribes,
             x="longitude",
