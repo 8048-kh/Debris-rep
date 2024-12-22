dcf4ec74b36d96feb8695c01cea99ea1d041c513
8048-kh <8046khtotn@gmail.com>
2024-12-22 21:44:43 +0800

    Update 999_測試.py

--- pages/999_測試.py
+++ pages/999_測試.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-import pandas as pd
+
 st.set_page_config(layout="wide")
 
-# Customize the sidebar
 markdown = """
-Web App URL: <https://geotemplate.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
 """
 
 st.sidebar.title("About")
@@ -14,29 +13,18 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-# Customize page title
-st.title("南投原鄉部落與土石流分布")
+st.title("Heatmap")
 
-st.markdown(
-    """
-    南投原鄉部落與土石流潛勢溪流、土石流潛勢溪流範圍分布
-    """
-)
-
-st.header("目錄")
-
-markdown = """
-1. 原鄉部落座標與資訊
-2. 原鄉部落與土石流潛勢溪流
-3. 原鄉部落與土石流潛勢溪流範圍
-
-"""
-
-st.markdown(markdown)
-
-
-m = leafmap.Map(center=[23.932630, 120.986852], zoom=10)
-#tribes = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Nantou_Tribe.csv"
-tribes_df = pd.read_csv("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Nantou_Tribe.csv")
-st.header("部落名稱")
-st.table(tribes_df[["tribe name"]])
+with st.expander("See source code"):
+    with st.echo():
+        filepath = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Debris_Points.csv"
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_heatmap(
+            filepath,
+            latitude="Y",
+            longitude="X",
+            value="TR_num",
+            name="Heat map",
+            radius=20,
+        )
+m.to_streamlit(height=700)
