7a2cf9a3d0d805f7fb61e3e3aca5e3a611bae96e
8048-kh <8046khtotn@gmail.com>
2024-12-23 18:58:53 +0800

    Update 999_測試.py

--- pages/999_測試.py
+++ pages/999_測試.py
@@ -3,17 +3,6 @@ import leafmap.foliumap as leafmap
 import pandas as pd
 import altair as alt
 st.set_page_config(layout="wide")
-
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
-
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
-
 st.title("Heatmap")
 
 with st.expander("See source code"):
